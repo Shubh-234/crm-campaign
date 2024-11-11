0296ebdf402b1c17f30eebddf5ae86453b95f0a1
Shubhankar <shubhankarbhanot@gmail.com>
2024-11-12 01:11:02 +0530

    added communication routes

---
+++ server/routes/communicationLogRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const router = express.router()
+
+const CommunicationLog = require('../models/CommunicationLog')
+
+router.post('/add',async (req,res)=> {
+    try {
+        const communicationLog = new CommunicationLog(req.body);
+        communicationLog.save();
+        res.status(201).json({message: 'Communication log added successfully',communicationLog});
+    } catch (error) {
+        res.status(500).json({message:'An error occurred while adding communication log',details:error.message});
+    }
+})
+
+router.get('/get',async (req,res)=> {
+    try {
+        const communicationLogs = CommunicationLog.find();
+        res.status(200).json(communicationLogs);
+    } catch (error) {
+        res.status(500).json({message: 'An error occurred while fetching communication logs',details:error.message});
+    }
+})
