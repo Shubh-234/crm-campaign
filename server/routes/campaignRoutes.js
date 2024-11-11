00ebf29b2dc5e0464c4f077973ea2b1773cf0341
Shubhankar <shubhankarbhanot@gmail.com>
2024-11-12 01:06:05 +0530

    added campaign routes

---
+++ server/routes/campaignRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const router = express.router()
+
+const Campaign = require('../models/Campaign');
+
+router.post('/add',async (req,res)=> {
+    try {
+        const campaign = new Campaign(req.body);
+        campaign.save();
+        res.status(201).json({message:'Added new campaign',campaign});
+    } catch (error) {
+        res.status(500).json({message:'An error occurred while adding campaigns',details: error.message});
+    }
+})
+
+router.get('/get',async (req,res)=> {
+    try {
+        const campaigns = Campaign.find();
+        res.status(200).json(campaigns);
+    } catch (error) {
+        res.status(500).json({message: 'An error occurred while fetching campaigns',details:error.message});
+    }
+})
